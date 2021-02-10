43e4d4f74c9b5f958c19f6e5e11a416057a35867
tatiana-belova <tan.belova2009@yandex.ru>
2021-02-10 13:11:33 +0300

    lesson 6

---
+++ main.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+from scrapy.crawler import CrawlerProcess
+from scrapy.settings import Settings
+from gb_parse.spiders.instagram import InstagramSpider
+
+if __name__ == "__main__":
+    load_dotenv(".env")
+    crawler_settings = Settings()
+    crawler_settings.setmodule("gb_parse.settings")
+    crawler_process = CrawlerProcess(settings=crawler_settings)
+    crawler_process.crawl(InstagramSpider, login=os.getenv("LOGIN"), password=os.getenv("PASSWORD"))
+    crawler_process.start()
